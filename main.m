6526de6e5107251d91419c529bc422cc3e81e7b8
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-11 01:33:38 -0300

    Acrescenta dados restantes

--- main.m
+++ main.m
@@ -1,14 +1,15 @@
 clear();
 clc();
 addpath(genpath("helpers_comuns"));
-
+%% QUANTIDADE DE PATAMARES / DISCRETIZAÇÃO TEMPORAL
 nT = 3;
 T  = ones(nT, 1);
-
 %% LIMITE CAPACIDADE BATERIA
 Cap = 500*T;
 %% PATAMARES DE CARGA
 Pd = [1.188; 2.960; 4.118];
+% GERAÇÃO FOTOVOLTAICA
+Pgsolar = [0 3.5 0];
 %% LIMITE DE POTÊNCIA MÁXIMA
 maxTermica = 7*T;
 maxBateria = 3*T;
@@ -25,6 +26,22 @@ bBateria = 0*T;
 Q        = diag(intercalar(aTermica, aBateria));
 b        = intercalar(bTermica, bBateria);
 
+%% RESTRIÇÃO DE IGUALDADE
+% Iaux*Pg + Pgsolar - Pd = 0
+Iaux = [
+    1  -1   0   0  0   0;
+    0   0   1  -1  0   0;
+    0   0   0   0  1  -1;
+];
+%% RESTRIÇÃO DESIGUALDADE
+%  -Pg + Pgmin <= 0
+%  Pg  - Pgmax <= 0
+%  -Mat_hor*Pg <= Cap
+Mat_hor = [
+    0   148   0     0   0   0;
+    0   148   0   428   0   0;
+    0   148   0   428   0 168;
+];
 
 
 
