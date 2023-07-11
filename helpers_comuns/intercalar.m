5ffeaab9a42319d6ebfb76e66fa69fa443604b76
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-11 04:55:23 -0300

    Comentário super útil em `intercalar`

--- helpers_comuns/intercalar.m
+++ helpers_comuns/intercalar.m
@@ -1,5 +1,5 @@
 function vetorIntercalado = intercalar(vetor1, vetor2)
-    vetor1 = vetor1(:)';
+    vetor1 = vetor1(:)'; % transforma em vetor-linha
     vetor2 = vetor2(:)';
     vetorIntercalado = [vetor1; vetor2];
     vetorIntercalado = vetorIntercalado(:);
